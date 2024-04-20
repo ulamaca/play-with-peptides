9b926ada549038f3d46ca8d4adea306f663ba7af
kuan <ulamaca.lee@gmail.com>
2024-04-20 22:19:33 +0800

    feat: added new type of cross_over

--- hill_climber.py
+++ hill_climber.py
@@ -1,11 +1,12 @@
 '''
     240416: addded crossover to increase diversity
+    240420: support a new cross_over: cut_inverted_cross_over
 '''
 
 import pandas as pd
 import random
 from manipulate.mutate import Genetic_Mutations
-from manipulate.cross_over import naive_cross_over, inverted_cross_over
+from manipulate.cross_over import naive_cross_over, cut_inverted_cross_over, random_inverted_cross_over
 from mlpep.actv_scorer import load_model, get_pred_result
 
 def legalize_seq_for_clf(seq: str):
@@ -33,7 +34,7 @@ if __name__ == "__main__":
     n_population = 20 # real poputation size = n_population * (1.3) + 1
     # mutate_rate = 0.5
     cross_over_rate = 0.8
-    cross_over_type = 'icv'
+    cross_over_type = 'c_icv'
     random.seed(42)
 
     seed_seq = 'TKPRPGP' # peptide: Selank
@@ -57,12 +58,18 @@ if __name__ == "__main__":
         score = df_best['p_actv'].item()        
         population = [legalize_seq_for_clf(mutator.mutate(best_step)) for _ in range(n_population-1)] + [best_step] 
 
+        # TODO: to sample survivors (seq being intact) according to its current score
+
         # cross over
         mates = [random.sample(population, 2) for _ in range(int(n_population*cross_over_rate))]
-        if cross_over_type == 'icv':
-            offsprings = [inverted_cross_over(parent[0], parent[1]) for parent in mates]
+        
+        if cross_over_type == 'r_icv':
+            offsprings = [random_inverted_cross_over(parent[0], parent[1]) for parent in mates]
+        elif cross_over_type == 'c_icv':
+            offsprings = [cut_inverted_cross_over(parent[0], parent[1]) for parent in mates]
         elif cross_over_type == 'naive':
             offsprings = [naive_cross_over(parent[0], parent[1]) for parent in mates]
+        
         offsprings = [legalize_seq_for_clf(c) for c in offsprings]
         population.extend(offsprings)    
         population = list(set(population) ) # make them unique()
